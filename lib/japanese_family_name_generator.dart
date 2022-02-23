ac6365b743b1a20756d44d3e85c020cce6e73eb3
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2022-02-23 11:59:10 +0900

    add functions generating Japanese kanji based family name

--- lib/japanese_family_name_generator.dart
+++ lib/japanese_family_name_generator.dart
@@ -1,1 +1,14 @@
 library japanese_family_name_generator;
+
+import 'dart:math';
+
+String generateKanjiCombinedFamilyNameText() => [
+      ['桜', '松', '竹', '梅', '神', '東', '西', '本', '大', '中', '小']
+          ._getRandomSingle(),
+      ['野', '原', '田', '畑', '川', '瀬', '木', '森', '林', '山', '村']
+          ._getRandomSingle(),
+    ].join();
+
+extension StringsExt on List<String> {
+  String _getRandomSingle() => this[Random().nextInt(length)];
+}
